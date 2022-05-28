c62939fbca44924b0fcc605640318068c66d638d
yayner <yayner2002@gmail.com>
2022-05-28 19:21:25 +0300

    Conditional rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,25 +6,19 @@ class Counter extends Component {
     tags: ["Tag 1", "Tag 2", "Tag 3", "Tag 4"],
   };
 
-  render() {
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>No Tags to Display</p>;
     return (
-      <div className="container">
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
-      </div>
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
     );
   }
 
-  getBadgeClasses() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-    return classes;
-  }
-
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+  render() {
+    return <div className="container">{this.renderTags()}</div>;
   }
 }
 
