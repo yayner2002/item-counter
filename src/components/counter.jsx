27d7dc60dddfeb59dfd3bd3ea7cc8faca6299281
yayner <yayner2002@gmail.com>
2022-05-29 14:09:17 +0300

    Make increment Button work

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,22 +3,48 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    tags: ["Tag 1", "Tag 2", "Tag 3", "Tag 4"],
   };
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>No Tags to Display</p>;
-    return (
-      <ul>
-        {this.state.tags.map((tag) => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
+  // renderTags() {
+  //   if (this.state.tags.length === 0) return <p>Please Add Your Tags</p>;
+  //   return (
+  //     <ul>
+  //       {this.state.tags.map((tag) => (
+  //         <li key={tag}>{tag}</li>
+  //       ))}
+  //     </ul>
+  //   );
+  // }
+  handleIncrement = () =>  {
+    this.setState({count:this.state.count + 1})
   }
 
   render() {
-    return <div className="container">{this.renderTags()}</div>;
+    return (
+      <div className="container">
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        {/* {this.state.tags.length === 0 && "No Tags to Show"}
+        {this.renderTags()} */}
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count ===0 ? 'Zero' : count;
+ 
   }
 }
 
